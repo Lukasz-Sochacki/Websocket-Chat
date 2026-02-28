bf01e60b6b07dafc4ec5d0f9ee3ca6f42543ab2b
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-02-28 22:52:54 +0100

    Finish basic app

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
+
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New Client! Its id - ' + socket.id);
+  socket.on('message', (message) => {
+    console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => {
+    console.log('Oh, socket ' + socket.id + ' has left');
+  });
+  console.log("I've added a listener on message event \n");
+});
 
 app.use(express.static(path.join(__dirname, './client')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './client/index.html'));
-});
-
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
 });
 
 const messages = [];
